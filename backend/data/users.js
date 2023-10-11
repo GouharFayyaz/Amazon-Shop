b64e871a95b2c36eb4dc373bc3b498a2952e544a
Gouhar Fayyaz <gfayyaz4@gmail.com>
2023-10-11 20:13:54 +0500

    first commit

---
+++ backend/data/users.js
@@ -0,0 +1,29 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'aliahmad@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Gouhar Fayyaz' ,
+    email: 'gfayyaz4@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Ali Ahmad',
+    email: 'ali@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+
+]
+
+export default users
